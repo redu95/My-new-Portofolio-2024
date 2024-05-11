ec8e5ebd2c3b954a4483f75058f0e97608e18a2f
redu95 <redietmuluken95@gmail.com>
2024-05-12 01:40:02 +0300

    Setting up tailwind

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className='container m-auto'>
+          <div className='flex'>
+            <div>
+              <h1>Rediet Portofolio</h1>
+            </div>
+            <div>
+              <ul>
+                <li>
+                  <a className='text-gray-400 hover:text-white'>Projects</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
