d85144ea4dc26d100698f03b77bedc5a6baf4a98
redu95 <redietmuluken95@gmail.com>
2024-05-13 19:57:02 +0300

    Adding Some Projects

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Profile from "./assets/Photo.png"
 import Project1 from "./assets/OnlineShopping.png"
 import Project2 from "./assets/CSEC ASTU.png"
-
+import Project3 from "./assets/ASTU UI_UX.png"
 
 function App() {
   return (
@@ -48,17 +48,46 @@ function App() {
         </section>
         {/* Projects Section */}
         <section className='comtainer m-auto'>
-          <h2>Projects</h2>
+          <h2 className='font-bold text-4xl mt-1'>Projects</h2>
           <div className='flex gap-10'>
-            <div className='border rounded-md p-5'>
-              <img src= {Project1}  />
+            <div className='border border-gray-500 rounded-md p-5'>
+              <img src= {Project1} className='w-full h-auto' />
               <h3 className='text-lg font-semi-bold mt-8'>Smart Shop Online Shopping</h3>
               <p className='text-gray-400 text-sm mt-2'>Responsive HTML/CSS Layout Project using Java Servlet,Jsp</p>
+              <div className='flex justify-evenely mt-12'>
+                <button className='px-6 py-3 bg-gradient-to-t from-purple-500 to-pink-500 hover:from-purple-700 hover:to-pink-700 border rounded-full'>Live Preview</button>
+                <button className='px-6 py-3 border rounded-full hover:border-purple-500 hover:text-purple-500'>Checkout github</button>
+              </div>
             </div>
-            <div className='border rounded-md p-5'>
-              <img src= {Project2}  />
+            <div className='border border-gray-500 rounded-md p-5'>
+              <img src= {Project2} className='w-full h-auto' />
               <h3 className='text-lg font-semi-bold mt-8'>Club Management System</h3>
               <p className='text-gray-400 text-sm mt-2'>Responsive HTML/CSS Layout Project using Nodejs, Expressjs</p>
+              <div className='flex justify-evenely mt-12'>
+                <button className='px-6 py-3 bg-gradient-to-t from-purple-500 to-pink-500 hover:from-purple-700 hover:to-pink-700 border rounded-full'>Live Preview</button>
+                <button className='px-6 py-3 border rounded-full hover:border-purple-500 hover:text-purple-500'>Checkout github</button>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex gap-10'>
+            <div className='border border-gray-500 rounded-md p-5'>
+              <img src= {Project3} className='w-full h-auto' />
+              <h3 className='text-lg font-semi-bold mt-8'>Redesigning ASTU Official Website</h3>
+              <p className='text-gray-400 text-sm mt-2'>Responsive UI/UX Design in Figma</p>
+              <div className='flex justify-evenely mt-12'>
+                <button className='px-6 py-3 bg-gradient-to-t from-purple-500 to-pink-500 hover:from-purple-700 hover:to-pink-700 border rounded-full'>Live Preview</button>
+                <button className='px-6 py-3 border rounded-full hover:border-purple-500 hover:text-purple-500'>Checkout github</button>
+              </div>
+            </div>
+            <div className='border border-gray-500 rounded-md p-5'>
+              <img src= {Project2} className='w-full h-auto' />
+              <h3 className='text-lg font-semi-bold mt-8'>Club Management System</h3>
+              <p className='text-gray-400 text-sm mt-2'>Responsive HTML/CSS Layout Project using Nodejs, Expressjs</p>
+              <div className='flex justify-evenely mt-12'>
+                <button className='px-6 py-3 bg-gradient-to-t from-purple-500 to-pink-500 hover:from-purple-700 hover:to-pink-700 border rounded-full'>Live Preview</button>
+                <button className='px-6 py-3 border rounded-full hover:border-purple-500 hover:text-purple-500'>Checkout github</button>
+              </div>
             </div>
           </div>
         </section>
